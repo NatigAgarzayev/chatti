a76cf69fd77229d0658d062042c9999cf391a391
NatigAgarzayev <natigagharzayev@gmail.com>
2024-07-01 21:13:38 +0400

    added email at the bottom

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,12 +1,23 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavLink from './NavLink'
 import { redirect, usePathname, useRouter } from 'next/navigation'
-import { userLogOut } from '@/api/authClient'
+import { getUserData, userLogOut } from '@/api/authClient'
+import { useUser } from '@/store/store'
 
 export default function Sidebar() {
     const router = useRouter()
     const pathname = usePathname()
+    const setUser = useUser(state => state.setUser)
+    const user = useUser(state => state.user)
+    useEffect(() => {
+        async function getUser() {
+            const user: any = await getUserData()
+            setUser(user)
+        }
+        getUser()
+    }, [])
+
 
     const logoutUser = async () => {
         await userLogOut()
@@ -14,7 +25,7 @@ export default function Sidebar() {
     }
 
     return (
-        <aside className="min-w-60 bg-gray-200">
+        <aside className="relative min-w-60 h-full bg-gray-200">
             <h1 className="text-center mt-4 font-bold text-xl">Chatti Tracking</h1>
             <ul>
                 {<li className={`mt-4 text-center p-4 rounded-sm ${pathname === "/dashboard/main" ? "bg-gray-400" : "bg-gray-300"} hover:bg-gray-400 cursor-pointer`}>
@@ -24,6 +35,9 @@ export default function Sidebar() {
                     Logout
                 </li>
             </ul>
+            <div className='absolute bottom-2 left-1/2 -translate-x-1/2 text-gray-900'>
+                {user?.email}
+            </div>
         </aside>
     )
 }
