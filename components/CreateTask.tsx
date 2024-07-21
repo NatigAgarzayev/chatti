86eff03cc15234c14fae0dedecce724461d72e57
NatigAgarzayev <natigagharzayev@gmail.com>
2024-07-22 01:24:46 +0400

    adsasdas

--- components/CreateTask.tsx
+++ components/CreateTask.tsx
@@ -16,8 +16,8 @@ export default function CreateTask() {
     const [quillText, setQuillText] = useState('')
     const user = useUser(state => state.user)
     const router = useRouter()
+
     const createTaskHandler = async () => {
-        // const { task } = Object.fromEntries(formData)
         if (quillText.toString().trim() === "") {
             alert("Fill required input")
             return
@@ -54,7 +54,6 @@ export default function CreateTask() {
                     <form action={createTaskHandler} className='min-w-40 w-full flex flex-col gap-4'>
                         {/* <textarea placeholder='Write smth' name="task" className='border-2 p-1 pl-3 py-2 outline-none border-gray-700 rounded-3xl'></textarea> */}
                         <ReactQuill theme="snow" value={quillText} onChange={setQuillText} modules={modules} formats={formats} />
-                        {/* <input placeholder='max 52 letters' maxLength={52} type="text" name='task' className='border-2 p-1 pl-3 py-2 outline-none border-gray-700 rounded-3xl' /> */}
                         <button disabled={createTaskLoading} className='p-2 bg-indigo-400 text-gray-700 font-bold rounded-3xl disabled:bg-indigo-200' type='submit'>{createTaskLoading ? "Process..." : "Create"}</button>
                     </form>
                 </DialogPanel>
