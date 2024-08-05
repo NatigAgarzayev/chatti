afa3653e21b4978fb6ab98ed8fad0c54569cb0f9
NatigAgarzayev <natigagharzayev@gmail.com>
2024-08-05 22:56:52 +0400

    modified timer input and end sound

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -11,19 +11,24 @@ import "moment-duration-format"
 import Head from 'next/head';
 export default function Timer() {
 
-    const endSound = new Audio("/audio/Alarm-Clock.mp3")
+    const endSound = new Audio("/audio/notify.mp3")
 
-    const [detail, setDetail] = useState<MaskEventDetail | null>(null);
-    const [val, setVal] = useState('')
-    const fRef = useRef<HTMLInputElement>(null)
     const [isTimerOn, setIsTimerOn] = useState(false)
     const [isPaused, setIsPaused] = useState(true)
     const [count, setCount] = useState(60)
     const [initialTime, setInitialTime] = useState(0)
+    const [values, setValues] = useState({ val1: '', val2: '', val3: '' })
+    const valRef1 = useRef<HTMLInputElement>(null)
+    const valRef2 = useRef<HTMLInputElement>(null)
+    const valRef3 = useRef<HTMLInputElement>(null)
 
     useEffect(() => {
-        if (fRef.current) {
-            fRef.current.focus()
+        console.log(values)
+    }, [values])
+
+    useEffect(() => {
+        if (valRef1.current) {
+            valRef1.current.focus()
         }
     }, [])
 
@@ -32,12 +37,13 @@ export default function Timer() {
         if (!isPaused) {
             interval = setInterval(() => {
                 setCount(prevCount => {
+                    if (prevCount === 1) {
+                        endSound.play()
+                    }
                     if (prevCount <= 0) {
                         clearInterval(interval)
-                        endSound.play()
                         setIsPaused(true)
                         setCount(initialTime)
-                        document.title = "End - Chatti"
                         return 0
                     }
                     return prevCount - 1
@@ -55,11 +61,10 @@ export default function Timer() {
     const handlePlay = () => {
         if (!isTimerOn) {
             setIsTimerOn(true)
-            const vals = detail?.value.split(" : ")
-            if (vals) {
-                const hours = +vals[0]
-                const minutes = +vals[1]
-                const seconds = +vals[2]
+            if (values) {
+                const hours = +values.val1
+                const minutes = +values.val2
+                const seconds = +values.val3
                 const res = hours * 3600 + minutes * 60 + seconds
                 setCount(res)
                 setInitialTime(res)
@@ -73,9 +78,9 @@ export default function Timer() {
         setIsTimerOn(false)
         setIsPaused(true)
         setCount(0)
-        setDetail(null)
-        if (fRef.current) {
-            fRef.current.focus()
+        setValues({ val1: '', val2: '', val3: '' })
+        if (valRef1.current) {
+            valRef1.current.focus()
         }
     }
 
@@ -98,16 +103,48 @@ export default function Timer() {
             </Head>
             {
                 !isTimerOn ?
-                    <InputMask
-                        ref={fRef}
-                        className='text-[96px] bg-transparent w-[525px] outline-none text-gray-700 text-center'
-                        mask="__ : __ : __"
-                        replacement={{ _: /\d/ }}
-                        value={detail?.value ?? ''}
-                        onMask={(event) => setDetail(event.detail)}
-                        onChange={() => setVal(detail?.value ?? '')}
-                        placeholder='00 : 00 : 00'
-                    />
+                    <div className='flex gap-4 items-center'>
+                        <input onKeyDown={(event) => {
+                            if (event.key === 'Enter' && isPaused && (values.val1 || values.val2 || values.val3)) {
+                                handlePlay()
+                            }
+                            if (event.key === 'Backspace' && !valRef1.current?.value) {
+                                valRef3.current?.focus()
+                            }
+                        }} ref={valRef1} value={values.val1} onChange={(e) => {
+                            setValues(prev => ({ ...prev, val1: e.target.value.replace(/[^0-9]/g, '') }))
+                            if (e.target.value.length === 2) {
+                                valRef2.current?.focus()
+                            }
+                        }
+
+                        } type="text" maxLength={2} className='text-[96px] text-center bg-transparent w-[125px] outline-none text-gray-700' placeholder='00' />
+                        <div className='text-[48px]'>:</div>
+                        <input onKeyDown={(event) => {
+                            if (event.key === 'Enter' && isPaused && (values.val1 || values.val2 || values.val3)) {
+                                handlePlay()
+                            }
+                            if (event.key === 'Backspace' && !valRef2.current?.value) {
+                                valRef1.current?.focus()
+                            }
+                        }} ref={valRef2} value={values.val2} onChange={(e) => {
+                            setValues(prev => ({ ...prev, val2: e.target.value.replace(/[^0-9]/g, '') }))
+                            if (e.target.value.length === 2) {
+                                valRef3.current?.focus()
+                            }
+                        }} type="text" maxLength={2} className='text-[96px] text-center bg-transparent w-[125px] outline-none text-gray-700' placeholder='00' />
+                        <div className='text-[48px]'>:</div>
+                        <input onKeyDown={(event) => {
+                            if (event.key === 'Enter' && isPaused && (values.val1 || values.val2 || values.val3)) {
+                                handlePlay()
+                            }
+                            if (event.key === 'Backspace' && !valRef3.current?.value) {
+                                valRef2.current?.focus()
+                            }
+                        }} ref={valRef3} value={values.val3} onChange={(e) => {
+                            setValues(prev => ({ ...prev, val3: e.target.value.replace(/[^0-9]/g, '') }))
+                        }} type="text" maxLength={2} className='text-[96px] text-center bg-transparent w-[125px] outline-none text-gray-700' placeholder='00' />
+                    </div>
                     :
                     <div className='text-[96px] text-gray-700' >
                         <DisplayTime />
@@ -122,7 +159,7 @@ export default function Timer() {
                 </div>
                 {
                     isPaused ?
-                        <button disabled={!detail?.isValid} onClick={handlePlay} className='p-4 rounded-3xl bg-indigo-400 w-fit cursor-pointer disabled:bg-indigo-200'>
+                        <button disabled={values.val1 === '' && values.val2 === '' && values.val3 === ''} type='submit' onClick={handlePlay} className='p-4 rounded-3xl bg-indigo-400 w-fit cursor-pointer disabled:bg-indigo-200'>
                             <Image src={playIcon} width={32} height={32} alt='' className='text-4xl' />
                         </button>
                         :
