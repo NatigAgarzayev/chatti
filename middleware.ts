773d80587dd1b76ec110c71bcd73a8325f45641c
NatigAgharzayev <natigagharzayev@gmail.com>
2024-10-06 00:13:48 +0300

    test 18

--- middleware.ts
+++ middleware.ts
@@ -8,11 +8,11 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/signin(.*)", "/signup(.*)", "api/webhooks"]);
 
-export default clerkMiddleware((auth, request) => {
+/* export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
     auth().protect();
   }
-});
+}); */
 
 export const config = {
   matcher: [
