2ddebe2c83c55509559df2c995080d10a4ff2617
NatigAgharzayev <natigagharzayev@gmail.com>
2024-10-06 00:25:12 +0300

    test 19

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,3 @@
-import { type NextRequest } from "next/server";
 // import { updateSession } from "@/utils/supabase/middleware";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
@@ -9,6 +8,10 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher(["/signin(.*)", "/signup(.*)"]);
 
 export default clerkMiddleware((auth, request) => {
+  if (request.nextUrl.pathname.startsWith("/api/webhooks")) {
+    // Bypass Clerk authentication for the /api/webhooks route
+    return;
+  }
   if (!isPublicRoute(request)) {
     auth().protect();
   }
@@ -20,6 +23,6 @@ export const config = {
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
-    // '/(api|trpc)(.*)',
+    '/(api|trpc)(.*)',
   ]
 };
