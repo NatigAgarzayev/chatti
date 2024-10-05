60c92fabca7f3982b6a97387048bbd6ea7d398e2
NatigAgharzayev <natigagharzayev@gmail.com>
2024-10-05 11:14:53 +0300

    stripe test

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,14 +1,16 @@
 import Stripe from 'stripe';
 import { NextRequest, NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs/dist/types/server';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 export async function POST(request: NextRequest) {
   try {
+    const { userId } = auth()
     const checkoutSession: Stripe.Checkout.Session =
       await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
         line_items: [
           {
-            price: "price_1Q6APGHP9wN4meCvIjuZ7YHo",
+            price: "price_1Q6T7kHP9wN4meCvX6zODHh8",
             quantity: 1,
           }
         ],
@@ -16,8 +18,8 @@ export async function POST(request: NextRequest) {
         success_url: 'https://chatti-jade.vercel.app/dashboard/habits',
         cancel_url: 'https://chatti-jade.vercel.app/dashboard/habits',
         metadata: {
-        //   userId: userId,
-          priceId: "price_1Q6APGHP9wN4meCvIjuZ7YHo",
+          userId: userId,
+          priceId: "price_1Q6T7kHP9wN4meCvX6zODHh8",
         }
       });
     return NextResponse.json({ result: checkoutSession, ok: true });
--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
+import { createClerkClient } from '@clerk/clerk-sdk-node'
+const clerkClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY as string })
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
-
-// You no longer export "handler"; export the HTTP method directly
 export async function POST(request: NextRequest) {
   try {
     const buf = await request.text(); // Get the raw body
@@ -14,9 +14,22 @@ export async function POST(request: NextRequest) {
 
     // Handle the event (for example, checkout.session.completed)
     if (event.type === 'checkout.session.completed') {
-      const session = event.data.object;
+      const session = event.data.object as Stripe.Checkout.Session;
       console.log(`Payment was successful for session ID: ${session.id}`);
       // Process the session or update your database here.
+      const userId = session.metadata?.userId;
+
+      if (userId) {
+        // Update Clerk user with payment information using clerkClient
+        await clerkClient.users.updateUser(userId, {
+          publicMetadata: {
+            paid: true, // Mark the user as having paid
+            paymentId: session.id, // Optionally, store the Stripe payment session ID
+          },
+        });
+      } else {
+        console.error('User ID not found in session metadata');
+      }
     }
 
     return NextResponse.json({ received: true });
