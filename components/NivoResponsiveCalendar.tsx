4cae334c055a659df4722e775387dd614501ab4b
Natig <natigagharzayev@gmail.com>
2025-01-04 14:32:12 +0200

    added year changing

--- components/NivoResponsiveCalendar.tsx
+++ components/NivoResponsiveCalendar.tsx
@@ -1,9 +1,9 @@
 'use client'
-import {ResponsiveCalendar} from '@nivo/calendar'
-import React, {useEffect, useState} from 'react'
-import {NivoDataset} from "@/types";
+import { ResponsiveCalendar } from '@nivo/calendar'
+import React, { useEffect, useState } from 'react'
+import { NivoDataset } from "@/types";
 
-export default function NivoResponsiveCalendar({ data }: { data: NivoDataset[] }) {
+export default function NivoResponsiveCalendar({ data, year }: { data: NivoDataset[], year: number }) {
 
     const [theme, setTheme] = useState("light")
 
@@ -11,39 +11,39 @@ export default function NivoResponsiveCalendar({ data }: { data: NivoDataset[] }
         if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
             document.documentElement.classList.add('dark');
             setTheme("dark")
-          } else {
+        } else {
             document.documentElement.classList.remove('dark');
             setTheme("light")
-          }
+        }
     }, [theme])
 
 
     return (
-            <ResponsiveCalendar
-                data={data}
-                from={`${new Date().getFullYear()}-01-01`}
-                to={`${new Date().getFullYear()}-12-31`}
-                emptyColor={theme === "light" ? "#eeeeee" : "#111827"}
-                colors={[theme === "light" ? 'rgb(99 102 241)' : "rgb(209 213 219)"]}
-                margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-                minValue={0}
-                maxValue='auto'
-                yearSpacing={40}
-                monthBorderColor={theme === "light" ? "#ffffff" : "rgb(75 85 99)"}
-                dayBorderWidth={2}
-                dayBorderColor={theme === "light" ? "#ffffff" : "rgb(75 85 99)"}
-                legends={[
-                    {
-                        anchor: 'bottom-right',
-                        direction: 'row',
-                        translateY: 36,
-                        itemCount: 4,
-                        itemWidth: 42,
-                        itemHeight: 36,
-                        itemsSpacing: 14,
-                        itemDirection: 'right-to-left'
-                    }
-                ]}
-            />
+        <ResponsiveCalendar
+            data={data}
+            from={`${year}-01-01`}
+            to={`${year}-12-31`}
+            emptyColor={theme === "light" ? "#eeeeee" : "#111827"}
+            colors={[theme === "light" ? 'rgb(99 102 241)' : "rgb(209 213 219)"]}
+            margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+            minValue={0}
+            maxValue='auto'
+            yearSpacing={40}
+            monthBorderColor={theme === "light" ? "#ffffff" : "rgb(75 85 99)"}
+            dayBorderWidth={2}
+            dayBorderColor={theme === "light" ? "#ffffff" : "rgb(75 85 99)"}
+            legends={[
+                {
+                    anchor: 'bottom-right',
+                    direction: 'row',
+                    translateY: 36,
+                    itemCount: 4,
+                    itemWidth: 42,
+                    itemHeight: 36,
+                    itemsSpacing: 14,
+                    itemDirection: 'right-to-left'
+                }
+            ]}
+        />
     )
 }
--- components/StatisticModal.tsx
+++ components/StatisticModal.tsx
@@ -1,12 +1,12 @@
 'use client'
-import {useStore} from '@/store/store'
-import {Description, Dialog, DialogPanel, DialogTitle} from '@headlessui/react'
-import React, {useEffect, useState} from 'react'
+import { useStore } from '@/store/store'
+import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import React, { useEffect, useState } from 'react'
 import NivoResponsiveCalendar from './NivoResponsiveCalendar'
-import {Habit} from '@/types'
-import {motion} from 'framer-motion'
+import { Habit } from '@/types'
+import { motion } from 'framer-motion'
 import Moment from "react-moment";
-import {getUserHabitById} from "@/api/habitClient";
+import { getUserHabitById } from "@/api/habitClient";
 import moment from "moment";
 
 
@@ -15,17 +15,18 @@ export default function StatisticModal({ dataId }: { dataId: number }) {
     const statisticModal = useStore(state => state.statisticModal)
     const updateStatisticModal = useStore(state => state.updateStatisticModal)
     const [noRecords, setNoRecords] = useState(false)
-    const [oneRecord, setOneRecord] = useState(false)
     const [stat, setStat] = useState<Habit>()
+    const [year, setYear] = useState(new Date().getFullYear())
+
 
     useEffect(() => {
         async function fetchStatistic() {
             try {
                 const res = await getUserHabitById({ id: dataId })
                     .then(res => {
-                        if(res) {
+                        if (res) {
                             setStat(res[0])
-                            if(res[0].records.length === 0){
+                            if (res[0].records.length === 0) {
                                 setNoRecords(true)
                             }
                         }
@@ -53,15 +54,21 @@ export default function StatisticModal({ dataId }: { dataId: number }) {
                         <DialogTitle className="font-bold dark:text-gray-100">
                             Analytics of {stat?.title + ' '}
                             ({stat?.type === 'timer' ?
-                            (noRecords ? <> Streak: <Moment diff={stat?.created_at} unit="days"/> days </>
+                                (noRecords ? <> Streak: <Moment diff={stat?.created_at} unit="days" /> days </>
 
                                     : 'Streak: ' + Math.max(stat?.streak, moment().diff(stat?.created_at, 'days')) + ' days')
-                            :
-                            'count: ' + stat?.count})
+                                :
+                                'count: ' + stat?.count})
                         </DialogTitle>
-                        <Description className="dark:text-gray-100">{`1 Jan ${new Date().getFullYear()} - 31 Dec ${new Date().getFullYear()}`}</Description>
+                        <Description className="dark:text-gray-100 flex items-center gap-2">
+                            <div onClick={() => setYear(year - 1)} className="cursor-pointer w-0 h-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-b-8 border-b-black transform rotate-[270deg]"></div>
+                            <p>
+                                {`1 Jan ${year} - 31 Dec ${year}`}
+                            </p>
+                            <div onClick={() => setYear(year + 1)} className="cursor-pointer w-0 h-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-b-8 border-b-black transform rotate-90"></div>
+                        </Description>
                         <div className='h-[200px] w-[1000px]'>
-                            <NivoResponsiveCalendar data={stat?.records || []} />
+                            <NivoResponsiveCalendar data={stat?.records || []} year={year} />
                         </div>
                     </DialogPanel>
                 </motion.div>
